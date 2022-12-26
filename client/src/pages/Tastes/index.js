18ae5006c5ca01f23ed8f152d6cd2e21b40b1f43
Chris <chrismanfredijr@gmail.com>
2022-12-26 16:20:23 -0500

    no tastes saved page

--- client/src/pages/Tastes/index.js
+++ client/src/pages/Tastes/index.js
@@ -5,7 +5,9 @@ import { removeTasteId } from '../../utils/localStorage';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../../utils/queries';
 import { REMOVE_TASTE } from '../../utils/mutations';
-import {  BsFillArrowUpRightSquareFill as ArrowLink } from 'react-icons/bs'
+import {BsHeadphones} from 'react-icons/bs'
+import {  BsFillArrowUpRightSquareFill as ArrowLink } from 'react-icons/bs';
+import {Link} from 'react-router-dom';
 
 const Tastes = () => {
 
@@ -40,37 +42,51 @@ const Tastes = () => {
     return <h2>LOADING...</h2>;
   }
   console.log(userData.tastes)
-  return (
-    userData.tastes.map((result, index) => (
-      <Card key={index} className="resultCard" >
-        <Card.Body className='resultBody'>
-          
-          <div className='artistImageWrapper'>
-            <img src={result.image} className="artistImage" alt={result.artist}></img>
-          </div>
+  if(userData.tastes.length === 0){
+    return (
+      userData.tastes.map((result, index) => (
+        <Card key={index} className="resultCard" >
+          <Card.Body className='resultBody'>
             
-          <div className='artistBioWrapper'>
-            <Card.Title>{result.artist}</Card.Title>
-              <Card.Text>
-                {result.bio}
-              </Card.Text>
-            <div className='cardLinks'>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteTaste(result.artistId)}>
-                    Remove Artist
-                  </Button>
-             
-                <a href={result.link} target="_blank" rel="noopener noreferrer" className='OpenLastFM' title="view artist on lastFM">
-                  <ArrowLink className='toLastFM' href={result.link} />
-                </a>
+            <div className='artistImageWrapper'>
+              <img src={result.image} className="artistImage" alt={result.artist}></img>
             </div>
-          </div>
-
-
-
-        </Card.Body>
-      </Card>
-    ))
-  );
+              
+            <div className='artistBioWrapper'>
+              <Card.Title>{result.artist}</Card.Title>
+                <Card.Text>
+                  {result.bio}
+                </Card.Text>
+              <div className='cardLinks'>
+                      <Button className='btn-block btn-danger' onClick={() => handleDeleteTaste(result.artistId)}>
+                      Remove Artist
+                    </Button>
+               
+                  <a href={result.link} target="_blank" rel="noopener noreferrer" className='OpenLastFM' title="view artist on lastFM">
+                    <ArrowLink className='toLastFM' href={result.link} />
+                  </a>
+              </div>
+            </div>
+  
+  
+  
+          </Card.Body>
+        </Card>
+      ))
+    );
+  }else{
+    return (
+      <div className='noTastes'>
+        <div className='noTasteContent'>
+          <h1>No tastes saved yet!</h1>
+          <h1 className='tasteHeadphones'><BsHeadphones/></h1>
+          <Button as={Link} to='/' variant="dark">Back to Search</Button>
+        </div>
+      </div>
+    )
+  }
+  
+  
 };
 
 export default Tastes;
